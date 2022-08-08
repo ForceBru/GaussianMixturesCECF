18bba37af02c08c9aaff00c30c8101dfffc96b32
ForceBru <ForceBru@users.noreply.github.com>
2022-08-09 01:16:16 +0300

    chore: delete parentheses around `TYPEDSIGNATURES`

--- src/Distance.jl
+++ src/Distance.jl
@@ -33,24 +33,6 @@ end
 """
 $(TYPEDSIGNATURES)
 
-Extract mixture parameters (weights, means and standard deviations)
-from a single vector of parameters.
-"""
-function get_mix_params(θ::AV{<:Real})
-    @assert length(θ) % 3 == 0
-
-    K = length(θ) ÷ 3 # number of components
-
-    p, mu, sigma = @views begin
-        θ[1:K], θ[K+1:2K], abs.(θ[2K+1:end])
-    end
-
-    (; p, mu, sigma)
-end
-
-"""
-$(TYPEDSIGNATURES)
-
 Same as `distance`, but all mixture parameters
 come from one vector. Used for optimization.
 """
--- src/Misc.jl
+++ src/Misc.jl
@@ -1,3 +1,21 @@
 "Normal PDF"
 @inline normal(x::Real, mu::Real, var::Real) =
     exp(-(x - mu)^2 / (2var)) / sqrt(2π * var)
+
+"""
+$(TYPEDSIGNATURES)
+
+Extract mixture parameters (weights, means and standard deviations)
+from a single vector of parameters.
+"""
+function get_mix_params(θ::AV{<:Real})
+    @assert length(θ) % 3 == 0
+
+    K = length(θ) ÷ 3 # number of components
+
+    p, mu, sigma = @views begin
+        θ[1:K], θ[K+1:2K], abs.(θ[2K+1:end])
+    end
+
+    (; p, mu, sigma)
+end
